c97ac9791eac6c36886bc43b6e996e1bc7045766
Joel Wakefield <joel.wakefield@improving.com>
2021-07-12 08:44:12 -0500

    step 3, tested

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -4,6 +4,54 @@ namespace FancyCalculator
 {
     class Program
     {
+
+        static dynamic IsNumber(string str, out double answer)
+        {
+            return Double.TryParse(str, out answer);
+        }
+
+        static bool RunCalculation(string input, out double answer)
+        {
+            var parts = input.Split(" ");
+
+            //  Check if the inputs are valid
+            var str1 = parts[0];
+            var str2 = parts[2];
+            double num1, num2;
+
+            if (IsNumber(str1, out num1) & IsNumber(str2, out num2))
+            {
+                switch (parts[1])
+                {
+                    case "+":
+                        answer = num1 + num2;
+                        break;
+                    case "-":
+                        answer = num1 - num2;
+                        break;
+                    case "*":
+                        answer = num1 * num2;
+                        break;
+                    case "/":
+                        answer = num1 / num2;
+                        break;
+                    case "%":
+                        answer = num1 % num2;
+                        break;
+                    default:
+                        Console.WriteLine("Lets try that again...");
+                        answer = 0;
+                        return false;
+                }
+            }
+            else
+            {
+                answer = 0;
+                return false;
+            }
+
+            return true;
+        }
         static dynamic QnA(string question)
         {
             do
@@ -12,20 +60,18 @@ namespace FancyCalculator
                 var result = Console.ReadLine();
                 double answer;
 
-                if (Double.TryParse(result, out answer))
+                if (RunCalculation(result, out answer))
                     return answer;
                 else
                     Console.WriteLine("Lets try that again...");
             } while (true);
-            
         }
-
+        
         static void Main(string[] args)
         {
-            var num1 = QnA("Give me a number: ");
-            var num2 = QnA("Give me a second number: ");
+            var answer = QnA("Give me an equation (please, separate the parts with a space): ");
 
-            Console.WriteLine($"Here's those numbers added together: {num1 + num2}");
+            Console.WriteLine($"Here's those numbers added together: {answer}");
             Console.ReadLine();
         }
     }
