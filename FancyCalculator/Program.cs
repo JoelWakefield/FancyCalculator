52d8301a669d0c4f47156eaa69c7555a2da7ebb2
Joel Wakefield <joel.wakefield@improving.com>
2021-07-12 08:25:42 -0500

    step 2, tested

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -4,13 +4,26 @@ namespace FancyCalculator
 {
     class Program
     {
+        static dynamic QnA(string question)
+        {
+            do
+            {
+                Console.WriteLine(question);
+                var result = Console.ReadLine();
+                double answer;
+
+                if (Double.TryParse(result, out answer))
+                    return answer;
+                else
+                    Console.WriteLine("Lets try that again...");
+            } while (true);
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a number: ");
-            var num1 = Double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Give me a second number: ");
-            var num2 = Double.Parse(Console.ReadLine());
+            var num1 = QnA("Give me a number: ");
+            var num2 = QnA("Give me a second number: ");
 
             Console.WriteLine($"Here's those numbers added together: {num1 + num2}");
             Console.ReadLine();
