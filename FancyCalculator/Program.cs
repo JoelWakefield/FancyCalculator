c62a288412cee0698729d03a7dbd1e6d0ce1218d
Joel Wakefield <joel.wakefield@improving.com>
2021-07-12 08:15:41 -0500

    step 1

---
+++ FancyCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FancyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");
+            var num1 = Console.ReadLine();
+
+            Console.WriteLine("Give me a second number: ");
+            var num2 = Console.ReadLine();
+
+            Console.WriteLine($"Here's those numbers added together: {num1 + num2}");
+            Console.ReadLine();
+        }
+    }
+}
