f77e69bab56c49c444483be6f251e546db3e4439
Joel Wakefield <joel.wakefield@improving.com>
2021-07-12 22:52:12 -0500

    fancyCalculator finshed with steps 1-13; the core lib needs more work though

--- FancyCalculator/Calculator.cs
+++ FancyCalculator/Calculator.cs
@@ -8,7 +8,7 @@ namespace FancyCalculator
 {
     class Calculator
     {
-        private List<Calculation> history = new List<Calculation>();
+        private List<Evaluation> history = new List<Evaluation>();
 
         dynamic IsNumber(string str, out double answer)
         {
@@ -25,7 +25,7 @@ namespace FancyCalculator
 
         void AppendHistory(double num1, string opp, double num2, double ans)
         {
-            history.Add(new Calculation
+            history.Add(new Evaluation
             {
                 Num1 = num1,
                 Num2 = num2,
@@ -36,29 +36,31 @@ namespace FancyCalculator
 
         bool RunCalculation(string input, ref double answer)
         {
+            Evaluation ev = new Evaluation();
+
             //  Break apart the input
             var parts = input.Split(" ");
 
             //  Setup varaibles
-            Calculation calculation = new Calculation();
-            double num1 = 0;
-            double num2 = 0;
-            string opp;
-            bool validInputs = false;
+            double num1;
+            double num2;
+            bool validInputs;
 
             //  Check to ensure the formatting is correct
             if (parts.Length == 3)
             {
-                opp = parts[1];
+                ev.Opperator = parts[1];
 
                 validInputs = IsNumber(parts[0], out num1) & IsNumber(parts[2], out num2);
 
                 //  Since this is starts a new calculation, reset history
                 history.Clear();
+
+                ev.IsHead = true;
             }
             else if (parts.Length == 2)
             {
-                opp = parts[0];
+                ev.Opperator = parts[0];
                 num1 = answer;
 
                 validInputs = IsNumber(parts[1], out num2);
@@ -75,7 +77,7 @@ namespace FancyCalculator
             if (validInputs)
             {
                 //  Check the opperator
-                switch (opp)
+                switch (ev.Opperator)
                 {
                     case "+":
                         answer = num1 + num2;
@@ -95,6 +97,10 @@ namespace FancyCalculator
                     default:
                         return Reject("Please, enter a valid opperator; ie, one listed here [ + , - , * , / , % ].");
                 }
+
+                ev.Num1 = num1;
+                ev.Num2 = num2;
+                ev.Answer = answer;
             }
             else
             {
@@ -102,17 +108,40 @@ namespace FancyCalculator
             }
 
             //  Append history
-            AppendHistory(num1, opp, num2, answer);
+            history.Add(ev);
 
             return true;
         }
 
-        void ShowHistory()
+        void ShowHistory(string input)
         {
+            //  Filter down requested operators
+            List<Evaluation> filteredHistory;
+            var inputParts = input.Split(" ");
+            if (inputParts.Length > 1)
+                filteredHistory = history.Where(e => e.Opperator == inputParts[1]).ToList();
+            else
+                filteredHistory = history;
+
             //  Get the longest string of commands
-            history.ForEach(c => {
-                Console.WriteLine(c);
+            var longestNum1 = 0;
+            var longestNum2 = 0;
+
+            filteredHistory.ForEach(e =>
+            {
+                if (e.Num1.ToString().Length > longestNum1)
+                    longestNum1 = e.Num1.ToString().Length;
+
+                if (e.Num2.ToString().Length > longestNum2)
+                    longestNum2 = e.Num2.ToString().Length;
             });
+
+            var tabSpaces = 8;
+            var tab1 = (longestNum1 / tabSpaces) + 1;
+            var tab2 = (longestNum2 / tabSpaces) + 1;
+
+            //  Write formatted results
+            filteredHistory.ForEach(e => Console.WriteLine(e.Result(tab1,tab2)));
         }
 
         public void Run()
@@ -129,12 +158,10 @@ namespace FancyCalculator
                 
                 if (result == "exit")   //  Check for quitters, lol
                     break;
-                else if (result == "history")   //  Show history
-                    ShowHistory();
+                else if (result.Contains("history"))   //  Show history
+                    ShowHistory(result);
                 else if (RunCalculation(result, ref answer)) //  Attempt calculation
-                {
                     Console.WriteLine($"{result} = {answer}");
-                }
                 else
                     Reject();
 
---
+++ FancyCalculator/Evaluation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyCalculator
+{
+    class Evaluation
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public string Opperator { get; set; }
+        public double Answer { get; set; }
+        public bool IsHead { get; set; } = false;   //  Is this the first evaluation in a sequence
+        public string ErrorMessage { get; set; }
+
+        public string Result(int tab1 = 1, int tab2 = 1)
+        {
+            if (String.IsNullOrWhiteSpace(ErrorMessage))
+                return $"{Format(Num1, tab1)}{Opperator} {Format(Num2,tab2,2)}= {Answer}";
+            else
+                return $"\u001b[31m{ErrorMessage}\u001b[0m";
+        }
+
+        private string Format(double num, int tabCount)
+        {
+            var tabSpaces = 8;
+            var msg = IsHead ? num.ToString() : $"_{num.ToString()}_";
+
+            for (int i = msg.ToString().Length; i < (tabCount * tabSpaces); i += tabSpaces)
+                msg += "\t";
+
+            return msg;
+        }
+
+        private string Format(double num, int tabCount, int extraSpace = 0)
+        {
+            var tabSpaces = 8;
+            var msg = num.ToString();
+
+            for (int i = msg.ToString().Length + extraSpace; i < (tabCount * tabSpaces); i += tabSpaces)
+                msg += "\t";
+
+            return msg;
+        }
+    }
+}
